b371e6b7bf79115e0153937caacce6112becffd4
komal65 <komalshingavi0302@gmail.com>
2024-08-28 19:38:18 +0530

    add fiels

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Handle form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
+
